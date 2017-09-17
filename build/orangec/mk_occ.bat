5f22e38791a9d086a6b4fccb289ba1d3af58c680
tenka <tenka@6809.net>
2017-09-18 00:12:58 +0900

    ビルドできないコンパイラのバッチを削除

--- build/orangec/mk_occ.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-set SRCDIR=..\..\src
-occ -DNDEBUG -I%SRCDIR% %SRCDIR%\genfnttex.cpp %SRCDIR%\FontGetter.cpp %SRCDIR%\misc\fks_fname.cpp %SRCDIR%\misc\mbc.c %SRCDIR%\misc\tga_wrt.c
-endlocal
